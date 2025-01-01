e25092676cdd9079110863f1f7f585db0608a723
@Ohrlando <ohrlando@gmail.com>
2025-01-01 16:14:55 -0300

    ajuste nome

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# LinhaInha
+# Loja Jamis
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.0.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'linha-inha'`, () => {
+  it(`should have as title 'loja-jamis'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('linha-inha');
+    expect(app.title).toEqual('loja-jamis');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('linha-inha app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('loja-jamis app is running!');
   });
 });
